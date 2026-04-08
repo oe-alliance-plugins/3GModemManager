ee67c328977c1bc7d8fe4a4554553c6dd2bba47b
jbleyel <jbleyel@gmx.net>
2026-04-08 10:51:46 +0200

    translation update

--- src/3GModemManager/__init__.py
+++ src/3GModemManager/__init__.py
@@ -6,15 +6,16 @@ from os.path import join
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 
-PLUGINPATH = resolveFilename(SCOPE_PLUGINS, "SystemPlugins/3GModemManager/")
+PluginLanguageDomain = "3GModemManager"
+PLUGINPATH = resolveFilename(SCOPE_PLUGINS, f"SystemPlugins/{PluginLanguageDomain}/")
 
 
 def localeInit():
-    bindtextdomain("3GModemManager", join(PLUGINPATH, "locale"))
+    bindtextdomain(PluginLanguageDomain, join(PLUGINPATH, "locale"))
 
 
 def _(txt):
-    t = dgettext("3GModemManager", txt)
+    t = dgettext(PluginLanguageDomain, txt)
     if t == txt:
         t = gettext(txt)
     return t
